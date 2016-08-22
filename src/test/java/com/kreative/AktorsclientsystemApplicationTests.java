4859441e6189c3ddd4dcab0c9a92d60940412dec
classy-madafaqa <ahmedd.abd.allah@gmail.com>
2016-08-23 01:57:18 +0200

    Created a new Maven Spring application

---
+++ src/test/java/com/kreative/AktorsclientsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kreative;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AktorsclientsystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
